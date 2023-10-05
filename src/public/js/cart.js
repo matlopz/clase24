eec9aa67e1b63debd42be4733eaf9ab9151760ad
matlopz <matlopez10@gmail.com>
2023-10-05 10:06:44 -0300

    actualizacion del nombre del usuario

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -1,14 +1,14 @@
 const fetchData = async () => {
     const authToken = localStorage.getItem('authToken');
     console.log('que tiene authToken', authToken);
-    
+
     if (!authToken) {
         console.log('No se encontró un token de autorización en el almacenamiento local.');
         return;
     }
-        showProducts();
-        setupAddToCartButtons();
-   
+    showProducts();
+    setupAddToCartButtons();
+
 };
 
 
@@ -33,9 +33,17 @@ const showProducts = async () => {
             return;
         }
 
+
         const producto = await response.json();
+        const nombre = document.createElement('h1')
+        console.log('que tiene nombre: ', nombre)
+        nombre.innerHTML = `
+        <h1>Bienvenido ${producto.usuario.name} !</h1>`;
+        productList.appendChild(nombre);
+
         producto.products.forEach(product => {
             const listItem = document.createElement('li');
+
             listItem.innerHTML = `
                 <br>----------------------------------------</br>
                 <strong>ID:</strong> ${product._id}<br>
@@ -67,8 +75,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-const setupAddToCartButtons = async() => {
- 
+const setupAddToCartButtons = async () => {
+
     const headers = {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
@@ -89,47 +97,47 @@ const setupAddToCartButtons = async() => {
 
         const producto = await response.json();
 
-        const {cartId} = producto
-                //const cartId = cartIdValue
-                console.log('carrito id:',cartId)
-    console.log('Valor del nuevo cart ID:', cartId);
-   
-    const addToCartForms = document.querySelectorAll('.addToCartForm');
-    addToCartForms.forEach(addToCartForm => {
-        addToCartForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
-            const productId = event.target.querySelector('button').getAttribute('data-product');
-            if (productId) {
-                try {
-   
-                    console.log(productId)
-                    const url = `/carts/${cartId}/product/${productId}`;
+        const { cartId } = producto
+        //const cartId = cartIdValue
+        console.log('carrito id:', cartId)
+        console.log('Valor del nuevo cart ID:', cartId);
 
-                    console.log('que tiene URL: ',url)
-                    const response = await fetch(url, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ productId, cartId }),
-                    });
+        const addToCartForms = document.querySelectorAll('.addToCartForm');
+        addToCartForms.forEach(addToCartForm => {
+            addToCartForm.addEventListener('submit', async (event) => {
+                event.preventDefault();
+                const productId = event.target.querySelector('button').getAttribute('data-product');
+                if (productId) {
+                    try {
 
-                    if (response.ok) {
-                        console.log('Producto agregado al carrito:', productId);
+                        console.log(productId)
+                        const url = `/carts/${cartId}/product/${productId}`;
 
-                    } else {
-                        console.log('Error al agregar el producto al carrito.');
+                        console.log('que tiene URL: ', url)
+                        const response = await fetch(url, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({ productId, cartId }),
+                        });
+
+                        if (response.ok) {
+                            console.log('Producto agregado al carrito:', productId);
+
+                        } else {
+                            console.log('Error al agregar el producto al carrito.');
+                        }
+                    } catch (error) {
+                        console.error('Error al agregar el producto al carrito:', error);
                     }
-                } catch (error) {
-                    console.error('Error al agregar el producto al carrito:', error);
+                } else {
+                    console.log('Error: No se pudo obtener el ID del producto.');
                 }
-            } else {
-                console.log('Error: No se pudo obtener el ID del producto.');
-            }
+            });
         });
-    });
-} catch (error) {
-    console.error('Error al realizar la solicitud:', error);
-}
+    } catch (error) {
+        console.error('Error al realizar la solicitud:', error);
+    }
 };
 
